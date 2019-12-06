193002097303c6768f81687d002026a58c204eeb
do_tha000 <do_tha000@CAM-LAB-20.bgsd.k12.wa.us>
2019-12-06 07:52:17 -0800

    New Save 1

--- FracCalc/src/fracCalc/FracCalc.java
+++ FracCalc/src/fracCalc/FracCalc.java
@@ -1,11 +1,19 @@
 package fracCalc;
-
+import java.util.*;
 public class FracCalc {
 
-    public static void main(String[] args) 
-    {
+    public static void main(String[] args) {
         // TODO: Read the input from the user and call produceAnswer with an equation
-
+    	Scanner console = new Scanner(System.in);
+    	boolean again = false;
+    	while(again) {
+    		String expression = console.nextLine();
+    		if (expression.equals("quit")) {
+    			again = true;
+    		}
+    		String op2 = produceAnswer(expression);
+    		System.out.println(op2);
+    	}
     }
     
     // ** IMPORTANT ** DO NOT DELETE THIS FUNCTION.  This function will be used to test your code
@@ -16,11 +24,28 @@ public class FracCalc {
     //        
     // The function should return the result of the fraction after it has been calculated
     //      e.g. return ==> "1_1/4"
-    public static String produceAnswer(String input)
-    { 
+    public static String produceAnswer(String input) { 
         // TODO: Implement this function to produce the solution to the input
-        
-        return "";
+    	/*int op1End = 0;
+    	int slash = 0;
+    	boolean twice = false;
+    	// Calculates when the first space or operator appears (a.k.a where the first operand ends)
+        while ((input.charAt(op1End).equals()input.' ') || (input.charAt(op1End) != '+') || (input.charAt(op1End) != '-') || (input.charAt(op1End) != '*')/* || !twice) {
+        	if (input.charAt(op1End) == '/') {
+        		slash++;
+        	}
+        	if(slash == 2) {
+        		twice = true;
+        	}
+        	op1End++;
+        }*/
+    	int op1End = input.indexOf(' ');
+        String operand1 = input.substring(0,op1End);
+        int opStart = op1End + 1;
+        String operator = input.substring(opStart, (opStart + 1));
+        int op2Start = opStart + 2;
+        String operand2 = input.substring(op2Start, input.length());
+        return operand2;
     }
 
     // TODO: Fill in the space below with any helper methods that you think you will need
